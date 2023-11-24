9e32ba76800d030825e24165578521691e671e0b
tlanier <thibaut.lanier@gmail.com>
2023-11-24 13:19:21 +0100

    Changement nom de fonction genetic.py

--- genetic.py
+++ genetic.py
@@ -6,19 +6,21 @@ MUTATION_PROBA = 0.2
 MUTATION_FACTOR = 0.5
 GENERATIONS = 10
 
-def fitness(path,wall):
-    a = path[-1]['hleft']
-    b = path[-1]['hright']
+
+def fitness(path, wall):
+    a = path[-1]["hleft"]
+    b = path[-1]["hright"]
     if a == None:
-        a=0
+        a = 0
     if b == None:
-        b=0
-    if is_winning_path(path,wall) :
+        b = 0
+    if is_winning_path(path, wall):
         print("OMG")
-        return 10-10*(len(path)/MAX_LENGTH)
-    if valid_path(path,wall) :
-        return (2-(2*len(wall)-(a+b))/(2*len(wall)))
+        return 10 - 10 * (len(path) / MAX_LENGTH)
+    if valid_path(path, wall):
+        return 2 - (2 * len(wall) - (a + b)) / (2 * len(wall))
     return 0
+
 
 def crossover(path1, path2):
     cut = randint(1, min(len(path1), len(path2)))
@@ -27,31 +29,34 @@ def crossover(path1, path2):
 
     return new_path_1, new_path_2
 
+
 def init_population(wall):
     population = [create_random_path(wall) for _ in range(POP_SIZE)]
     return population
 
+
 def fill_population(population, wall):
-    for _ in range(POP_SIZE-len(population)):
+    for _ in range(POP_SIZE - len(population)):
         population.append(create_random_path(wall))
     return population
 
-def fit_pop(population,wall):
+
+def fit_population(population, wall):
     fitted_pop = []
     for path in population:
-        fitted_pop.append(fitness(path,wall))
+        fitted_pop.append(fitness(path, wall))
     return fitted_pop
 
-def select_parents(population, fitness_scores,wall):
 
+def select_parents(population, fitness_scores, wall):
     selected_parents = []
 
     for _ in range(NUM_PARENTS):
-
         selected_index = choices(range(len(population)), weights=fitness_scores)
         selected_parents.append(population[selected_index[0]])
 
     return selected_parents
+
 
 def breed_population(parents):
     offspring = []
@@ -70,33 +75,34 @@ def breed_population(parents):
 
     return offspring
 
-def mutate(population,wall):
-    for path in population :
-        if random() < MUTATION_PROBA :
-            mutation_index = randint(int(len(path)*MUTATION_FACTOR),len(path)-1)
-            mutation_size = randint(mutation_index,MAX_LENGTH)
+
+def mutate(population, wall):
+    for path in population:
+        if random() < MUTATION_PROBA:
+            mutation_index = randint(int(len(path) * MUTATION_FACTOR), len(path) - 1)
+            mutation_size = randint(mutation_index, MAX_LENGTH)
             path = path[:mutation_index]
             while len(path) <= mutation_size:
-                path = random_extend(path,wall)
+                path = random_extend(path, wall)
     return population
+
 
 def algo_genetique():
     population = init_population(wall)
 
-
     for generation in range(GENERATIONS):
         print(f"Generation : {generation}")
-        fitness_scores = fit_pop(population,wall)
-        parents = select_parents(population, fitness_scores,wall)
+        fitness_scores = fit_population(population, wall)
+        parents = select_parents(population, fitness_scores, wall)
         offspring = breed_population(parents)
-        offspring = [i for i in offspring if valid_path(i,wall)]
+        offspring = [i for i in offspring if valid_path(i, wall)]
         offspring = fill_population(offspring, wall)
-        offspring = mutate(offspring,wall)
+        offspring = mutate(offspring, wall)
         population = offspring
-
 
     best_solution = population[fitness_scores.index(max(fitness_scores))]
     print(best_solution)
     return best_solution
 
-algo_genetique()
+
+path_to_json(algo_genetique())
