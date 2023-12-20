4e6235b83408f7f604d2eef1eae752a15db2bf87
vthierry <thierry.vieville@inria.fr>
2023-12-20 15:47:13 +0100

    just a few modif in script.sh

--- script.sh
+++ script.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
+
 pip install -r requirements.txt
 
-flask --app app run &> /dev/null
+xterm -T 'Generateur de chemin Genetique' -e flask --app app run &
 
-xdg-open index.html &> /dev/null
+xdg-open index.html"?wall=`cat data/wall/wall_test.json`&initPos={x:??,y:??}&run_genetic"
 
 # sleep 10
 
 touch /tmp/text.txt
 echo "*** INSTRUCTIONS ***" > /tmp/text.txt
-echo 'Clic on "importer mur"' >> /tmp/text.txt
-echo 'Select the wall "data/wall/wall_test.json"' >> /tmp/text.txt
+#echo 'Clic on "importer mur"' >> /tamp/text.txt
+#echo 'Select the wall "data/wall/wall_test.json"' >> /tmp/text.txt
 echo 'move the person' >> /tmp/text.txt
 echo 'clic on "Chemin génétique"' >> /tmp/text.txt
-
 gedit /tmp/text.txt
 
